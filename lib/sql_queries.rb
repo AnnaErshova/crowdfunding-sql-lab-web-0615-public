8e34d8b2caf5df6bb2e726c9b90527cb7fee656c
AnnaErshova <anna.ershova@gmail.com>
2015-06-17 18:01:59 -0400

    refactors code

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -1,24 +1,30 @@
-# Write your sql queries in this file in the appropriate method like the example below:
-# 
-# def select_category_from_projects
-# "SELECT category FROM projects;"
-# end
-
-# Make sure each ruby method returns a string containing a valid SQL statement.
+# users <= pledges => projects
+# projects.id == pledges.project_id 
+# pledges.user_id = users.id 
 
 def selects_the_titles_of_all_projects_and_their_pledge_amounts
-  "SELECT title, SUM(amount) FROM (SELECT * FROM projects INNER JOIN pledges ON pledges.project_id = projects.id) GROUP BY title;"
+  "SELECT projects.title, SUM(pledges.amount) 
+    FROM projects INNER JOIN pledges 
+       ON pledges.project_id = projects.id
+        GROUP BY projects.title;"
   # merges projects and pledges on project_id 
   # this assumes each project has more than one pledge, so have to use SUM
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges
-  "SELECT name, age, SUM(amount) FROM (SELECT * FROM users INNER JOIN pledges ON pledges.user_id = users.id) GROUP BY name;"
+  "SELECT users.name, users.age, SUM(pledges.amount) 
+    FROM users INNER JOIN pledges 
+      ON pledges.user_id = users.id 
+        GROUP BY users.name;"
   # merges users and pledges on user_id
 end
 
 def selects_the_titles_of_all_projects_that_have_met_their_funding_goal
-  "SELECT * FROM (SELECT title, funding_goal - SUM(amount) AS delta FROM (SELECT * FROM projects INNER JOIN pledges ON pledges.project_id = projects.id) GROUP BY title) WHERE delta <= 0;"
+  "SELECT * FROM 
+    (SELECT projects.title, projects.funding_goal - SUM(pledges.amount) AS delta 
+      FROM projects INNER JOIN pledges 
+        ON pledges.project_id = projects.id 
+          GROUP BY title) WHERE delta <= 0;"
   # merges projects and pledges
   # selects both titles and delta from w/funding goal as per rspec -- this should have a different method name....
   # delta is difference between funding goal and amount raised 
@@ -27,17 +33,28 @@ def selects_the_titles_of_all_projects_that_have_met_their_funding_goal
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount
-  "SELECT name, SUM(amount) FROM (SELECT * FROM users INNER JOIN pledges ON pledges.user_id = users.id) GROUP BY name ORDER BY SUM(amount) ASC;"
+  "SELECT users.name, SUM(pledges.amount) 
+    FROM users INNER JOIN pledges 
+      ON pledges.user_id = users.id 
+        GROUP BY users.name 
+          ORDER BY SUM(pledges.amount);"
   # merges users and pledges
   # I originally ordered just by amount, but it needed to be ordered by SUM of the amount
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
-  "SELECT category, amount FROM (SELECT * FROM projects INNER JOIN pledges on pledges.project_id = projects.id WHERE category = 'music');"
+  "SELECT projects.category, pledges.amount 
+    FROM projects INNER JOIN pledges 
+      ON pledges.project_id = projects.id 
+        WHERE projects.category = 'music';"
   # merges projects and pledges
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_book_category
-  "SELECT category, SUM(amount) FROM (SELECT * FROM projects INNER JOIN pledges on pledges.project_id = projects.id WHERE category = 'books');"
+  "SELECT projects.category, SUM(pledges.amount) 
+    FROM projects INNER JOIN pledges 
+      ON pledges.project_id = projects.id 
+        WHERE projects.category = 'books';"
   # merges projects and pledges 
 end
+
